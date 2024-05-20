a7cb70f85e270e2dadf82277ef07b28f691074f0
Jackson Garland <jgarland@zytron.net>
2024-05-20 15:35:53 -0500

    skeleton of command structure

--- cogchat/cogchat.py
+++ cogchat/cogchat.py
@@ -21,4 +21,54 @@ class CogChat(commands.Cog):
             force_registration=True,
         )
         
-    
+    @commands.guild_only()
+    @commands.command(name="cogchat")
+    async def cogchat(self, ctx, *, text: str):
+        """Base command for CogChat."""
+        command_array = text.split(" ")
+        
+        match command_array[0]:
+            case "register":
+                # create an entry in storage for the current guild (if not already existing) and channel.
+                pass
+            case "start":
+                # begin listening for new messages to pass to LLM
+                pass
+            case "stop":
+                # stop listening for new messages to pass to LLM
+                pass
+            case "configure":
+                try:
+                    match command_array[1]:
+                        case "character":
+                            pass
+                        case "temperature":
+                            pass
+                        case "top_p":
+                            pass
+                        case "max_tokens":
+                            pass
+                        case "seed":
+                            pass
+                except Exception: # too few arguments
+                    await ctx.send("Usage ...") # fill in usage information
+            case "character":
+                try:
+                    match command_array[1]:
+                        case "create":
+                            pass
+                        case "delete":
+                            pass
+                        case "persona":
+                            match command_array[2]:
+                                case "show":
+                                    pass
+                                case "new":
+                                    pass
+                            
+                except Exception: # too few arguments
+                    await ctx.send("Usage ...") # fill in usage information
+            
+        
+
+        
