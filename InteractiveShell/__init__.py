6770920ddfc02168fc4a8d469550d06535b9555e
JackBinary <JMG.EXE@gmail.com>
2025-06-20 14:02:28 -0500

    Update __init__.py

--- InteractiveShell/__init__.py
+++ InteractiveShell/__init__.py
@@ -1,3 +1,5 @@
+"""Setup module for the InteractiveShell cog."""
+
 import json
 from pathlib import Path
 
@@ -5,8 +7,9 @@ from redbot.core.bot import Red
 
 from .main import InteractiveShell
 
-with open(Path(__file__).parent / "info.json") as fp:
+with open(Path(__file__).parent / "info.json", encoding="utf-8") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 async def setup(bot: Red) -> None:
+    """Add the InteractiveShell cog to the bot."""
     await bot.add_cog(InteractiveShell(bot))
