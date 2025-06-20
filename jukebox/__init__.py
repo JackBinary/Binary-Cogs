e9fb18d5d07f1157cbc0258c44c51624983c9779
JackBinary <JMG.EXE@gmail.com>
2025-06-20 15:36:42 -0500

    Update __init__.py

--- jukebox/__init__.py
+++ jukebox/__init__.py
@@ -1,3 +1,4 @@
+"""Play music from a local file"""
 import json
 from pathlib import Path
 
@@ -5,8 +6,9 @@ from redbot.core.bot import Red
 
 from .jukebox import Jukebox
 
-with open(Path(__file__).parent / "info.json") as fp:
+with open(Path(__file__).parent / "info.json", encoding="utf-8") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 async def setup(bot: Red) -> None:
+    """Setup module for the Jukebox cog."""
     await bot.add_cog(Jukebox(bot))
