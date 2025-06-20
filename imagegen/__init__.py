64fd6a31982eb5731c941ae2e2dc210a70ccda7a
JackBinary <JMG.EXE@gmail.com>
2025-06-20 14:48:17 -0500

    pylint compliance

--- imagegen/__init__.py
+++ imagegen/__init__.py
@@ -1,3 +1,5 @@
+"""Initialize the ImageGen cog and load metadata for Red."""
+
 import json
 from pathlib import Path
 
@@ -5,8 +7,10 @@ from redbot.core.bot import Red
 
 from .imagegen import ImageGen
 
-with open(Path(__file__).parent / "info.json") as fp:
+# Load end user data statement from info.json
+with open(Path(__file__).parent / "info.json", encoding="utf-8") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 async def setup(bot: Red) -> None:
+    """Load the ImageGen cog into the bot."""
     await bot.add_cog(ImageGen(bot))
